f057629ba6c8de9226d902cede8442e291308e45
sayedhany <sayedh@mail.ru>
2023-05-25 05:39:19 -0700

    solved

--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
-#ifndef MONYT_H
-#define MONTY_H
+#ifndef __HOOLB__
+#define __HOOLB__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,7 +18,7 @@
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
-  */
+ */
 typedef struct stack_s
 {
 	int n;
@@ -38,7 +39,20 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_num);
 } instruction_t;
+
+void pint(stack_t **stack, unsigned int line_num);
+void free_stack(stack_t *head);
+int pushint(char *list, int ln);
 int _strcmp(char *opcode, char *list);
+void add(stack_t **stack, unsigned int line_num);
+void swap(stack_t **stack, unsigned int line_num);
+void pop(stack_t **stack, unsigned int line_num);
 void push(stack_t **stack, unsigned int line_number, int n);
 void pall(stack_t **stack, unsigned int line_num);
+void execute(char *string[], stack_t *stack);
+int nlfind(char *list);
+void free_list(char *a[]);
+int combfind(char *list, int ln);
+
+
 #endif
